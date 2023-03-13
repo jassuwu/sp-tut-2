655f8236f98afcee6b2be603cfeab8d65a3a0544
jassuwu <20pw14@psgtech.ac.in>
2023-03-13 08:42:58 +0530

    changes cors access

--- packages/server/app.js
+++ packages/server/app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "http://localhost:3000/" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
